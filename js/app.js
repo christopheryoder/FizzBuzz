3a39628567191ec88c33e8dc7c09e369b8f67532
christopheryoder <christophercyoder@gmail.com>
2014-09-05 14:56:42 -0400

    Added User Input and refined if then logic

--- js/app.js
+++ js/app.js
@@ -1,20 +1,57 @@
 $(document).ready(function() {
-	var i = 1;
-	while(i <= 100) {
-		if (i%3==0) {
-			if (i%5==0) {
-				$("body").append("fizzbuzz<br>")
-			}
-			else {
-			$("body").append("fizz<br>");
-			}
+	var numberAlert = function(number) {
+		if (isNaN(number)||number % 1 !== 0) {
+			alert("Please enter a non-decimal number (e.g. \"42\")");
 		}
-		else if (i%5==0) {
-			$("body").append("buzz<br>");
-		}
-		else {
-			$("body").append(i + "<br>");
-		};
-		i++
 	};
+
+	var fizzBuzz = function(userNumber) {
+		var i = 1;
+		while(i <= userNumber) {
+			if (i%3===0&&i%5===0) {
+				$("body").append("<br>fizzbuzz");
+			}
+
+			else if (i%3===0) {
+			$("body").append("<br>fizz");
+			}
+
+			else if (i%5===0) {
+				$("body").append("<br>buzz");
+			}
+
+			else {
+				$("body").append("<br>" + i);
+			}
+			i++;
+		}
+	};
+	$('input:text').keyup(function(event){
+        if(event.keyCode == 13) {
+
+            /*get user input*/
+            var userNumber = +$(this).val();
+
+            numberAlert(userNumber);
+
+            /*run FizzBuzz*/
+            fizzBuzz(userNumber);
+
+            /*delete contents of text box after user hits enter*/
+            $(this).val('');
+        }
+   	});
+   	$('input:submit').click(function() {
+
+        /*get user input*/
+        var userNumber = +$(this).siblings('input:text').val();
+
+        numberAlert(userNumber);
+
+        /*run FizzBuzz*/
+        fizzBuzz(userNumber);
+
+        /*delete contents of text box after user clicks 'add'*/
+        $(this).siblings('input:text').val('');
+    });
 });
